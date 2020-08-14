eac230ddca2d0e657b3c734c38dcaeb6ff912f2a
Vitche Research Team <developer@vitche.com>
2020-08-14 11:31:23 -0400

    1. Executing commands in a sequence.

--- main.js
+++ main.js
@@ -9,44 +9,50 @@ module.exports = {
         for (let index in clusterDefinition.nodes) {
 
             let node = clusterDefinition.nodes[index];
-            command = Buffer.from(command).toString('base64');
-            node.parameters = [command];
+            const commandBuffer = Buffer.from(command).toString('base64');
+            node.parameters = [commandBuffer];
 
             const nodeIdentifier = node.identifier;
             let requestBody = JSON.stringify(node);
-
             const job = function () {
                 const fetch = require('node-fetch');
                 requestBody = JSON.parse(requestBody);
-                const command = requestBody.parameters[0];
-                requestBody.parameters[0] = Buffer.from(command, 'base64').toString();
+                requestBody.parameters[0] = Buffer.from(requestBody.parameters[0], 'base64').toString();
                 requestBody = JSON.stringify(requestBody);
                 fetch(gatewayUri, {
                     method: 'post',
                     body: requestBody,
                     headers: {'Content-Type': 'application/json'},
                 })
-                    .then(response => response.json())
+                    .then((response) => {
+                        return response.json();
+                    })
                     .then((json) => {
-                        console.log('Node #' + nodeIdentifier + ' response:', json.output);
+                        if (json.output) {
+                            console.log('Node #' + nodeIdentifier + ' response:', json.output);
+                        }
+                        if (json.error) {
+                            console.log('Node #' + nodeIdentifier + ' error:', json.error);
+                        }
                     });
             };
-            const code = cronScript.compile(job, {
-                "gatewayUri": gatewayUri,
-                "requestBody": requestBody,
-                "nodeIdentifier": nodeIdentifier
-            });
-            const callback = function (error, stdout, stderr) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log(stdout, stderr);
-                }
-            };
             if (schedulerPattern) {
+                const code = cronScript.compile(job, {
+                    "gatewayUri": gatewayUri,
+                    "requestBody": requestBody,
+                    "nodeIdentifier": nodeIdentifier
+                });
+                const callback = function (error, standardOutput, standardError) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log(standardOutput, standardError);
+                    }
+                };
                 cronScript.schedule(nodeIdentifier, schedulerPattern, code, callback);
             } else {
-                cronScript.execute(code, callback);
+                // cronScript.execute(code, callback);
+                job();
             }
         }
     }
