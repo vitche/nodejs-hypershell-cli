87ba71d202ab2af9c12837d7c713430a6e7a5e00
Vitche Research Team <developer@vitche.com>
2021-04-23 19:58:21 +0300

    1. HySh Manager mode for the CLI and parsing respective arguments.

--- cli.js
+++ cli.js
@@ -1,20 +1,82 @@
 #!/usr/bin/env node
-let fs = require('fs');
-let main = require('./main');
+const fs = require("fs");
+const main = require("./main");
+const stdio = require("stdio");
+
+let toolCommand = process.argv[1];
+
+// 0
+// 1
+// 2 - cluster definition
+// 3 - command or shell file
+// 4 - [format] key
+// 5 - [format] value
 if (4 > process.argv.length) {
-    console.log('Usage: hysh cluster.json "uptime; who; pwd;" [--format stream|stream+json]');
+  console.log(
+    'Usage:\n' +
+    'hysh cluster.json "uptime; who; pwd;" [--format stream|stream+json]\n' +
+    'hyshm cluster.json [install|instantiate] --organization-identifier={organizationIdentifer} --administrator-logon={administratorLogOn} --administrator-password={administratorPassword} --chaincode-name={chainCodeName} --chaincode-identifier={chainCodeIdentifier} --chaincode-version={chainCodeVersion}');
+} else if (0 < toolCommand.indexOf("hyshm")) {
+  // "hyshm" mode
+  const options = stdio.getopt({
+    "organization-identifier": {
+      args: 1,
+      required: true
+    },
+    "administrator-logon": {
+      args: 1,
+      required: true
+    },
+    "administrator-password": {
+      args: 1,
+      required: true
+    },
+    "chaincode-name": {
+      args: 1,
+      required: true
+    },
+    "chaincode-identifier": {
+      args: 1,
+      required: true
+    },
+    "chaincode-version": {
+      args: 1,
+      required: true
+    }
+  });
+
+  if (options.args) {
+    let clusterDefinitionPath = options.args[0];
+    let command = options.args[1];
+    delete options.args;
+    switch (command) {
+      case "install": {
+        let clusterDefinition = fs.readFileSync(clusterDefinitionPath);
+        clusterDefinition = JSON.parse(clusterDefinition.toString());
+        main.install(clusterDefinition, options);
+        break;
+      }
+      case "instantiate": {
+        let clusterDefinition = fs.readFileSync(clusterDefinitionPath);
+        clusterDefinition = JSON.parse(clusterDefinition.toString());
+        main.instantiate(clusterDefinition, options);
+        break;
+      }
+    }
+  }
 } else {
-    let clusterDefinitionPath = process.argv[2];
-    let shellCommand = process.argv[3];
-    let formatKey = process.argv[4];
-    let formatValue = process.argv[5];
-    if (!("--format" === formatKey && "stream+json" === formatValue)) {
-        formatValue = "stream";
-    }
-    if (fs.existsSync(shellCommand)) {
-        shellCommand = fs.readFileSync(shellCommand).toString();
-    }
-    let clusterDefinition = fs.readFileSync(clusterDefinitionPath);
-    clusterDefinition = JSON.parse(clusterDefinition.toString());
-    main.execute(clusterDefinition, shellCommand, formatValue);
+  // "hysh" mode
+  let clusterDefinitionPath = process.argv[2];
+  let shellCommand = process.argv[3];
+  let formatKey = process.argv[4];
+  let formatValue = process.argv[5];
+  if (!("--format" === formatKey && "stream+json" === formatValue)) {
+    formatValue = "stream";
+  }
+  if (fs.existsSync(shellCommand)) {
+    shellCommand = fs.readFileSync(shellCommand).toString();
+  }
+  let clusterDefinition = fs.readFileSync(clusterDefinitionPath);
+  clusterDefinition = JSON.parse(clusterDefinition.toString());
+  main.execute(clusterDefinition, shellCommand, formatValue);
 }
--- main.js
+++ main.js
@@ -1,67 +1,70 @@
 const cronScript = require('nodejs-cron-script');
 module.exports = {
-    execute: function (clusterDefinition, command, outputFormat) {
-        const gatewayUri = clusterDefinition["gateway"]["uri"];
-        let schedulerPattern = undefined;
-        if (clusterDefinition["scheduler"]) {
-            schedulerPattern = clusterDefinition["scheduler"]["pattern"];
-        }
-        for (let index in clusterDefinition.nodes) {
-
-            let node = clusterDefinition.nodes[index];
-            const commandBuffer = Buffer.from(command).toString('base64');
-            node.parameters = [commandBuffer];
-
-            const nodeIdentifier = node.identifier;
-            let requestBody = JSON.stringify(node);
-            const job = function () {
-                const fetch = require('node-fetch');
-                requestBody = JSON.parse(requestBody);
-                requestBody.parameters[0] = Buffer.from(requestBody.parameters[0], 'base64').toString();
-                requestBody = JSON.stringify(requestBody);
-                fetch(gatewayUri, {
-                    method: 'post',
-                    body: requestBody,
-                    headers: {'Content-Type': 'application/json'},
-                })
-                    .then((response) => {
-                        return response.json();
-                    })
-                    .then((json) => {
-                        switch (outputFormat) {
-                            case "stream":
-                                if (json.output) {
-                                    console.log(nodeIdentifier + ':$', json.output);
-                                }
-                                if (json.error) {
-                                    console.log(nodeIdentifier + ':&2$', json.error);
-                                }
-                                break;
-                            case "stream+json":
-                                console.log(JSON.stringify(json) + ",");
-                                break;
-                        }
-
-                    });
-            };
-            if (schedulerPattern) {
-                const code = cronScript.compile(job, {
-                    "gatewayUri": gatewayUri,
-                    "requestBody": requestBody,
-                    "nodeIdentifier": nodeIdentifier
-                });
-                const callback = function (error, standardOutput, standardError) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        console.log(standardOutput, standardError);
-                    }
-                };
-                cronScript.schedule(nodeIdentifier, schedulerPattern, code, callback);
-            } else {
-                // cronScript.execute(code, callback);
-                job();
-            }
-        }
+  execute: function (clusterDefinition, command, outputFormat) {
+    const gatewayUri = clusterDefinition["gateway"]["uri"];
+    let schedulerPattern = undefined;
+    if (clusterDefinition["scheduler"]) {
+      schedulerPattern = clusterDefinition["scheduler"]["pattern"];
     }
+    for (let index in clusterDefinition.nodes) {
+      let node = clusterDefinition.nodes[index];
+      const commandBuffer = Buffer.from(command).toString('base64');
+      node.parameters = [commandBuffer];
+      const nodeIdentifier = node.identifier;
+      let requestBody = JSON.stringify(node);
+      const job = function () {
+        const fetch = require('node-fetch');
+        requestBody = JSON.parse(requestBody);
+        requestBody.parameters[0] = Buffer.from(requestBody.parameters[0], 'base64').toString();
+        requestBody = JSON.stringify(requestBody);
+        fetch(gatewayUri + "/chainCode/invoke", {
+          method: 'post',
+          body: requestBody,
+          headers: {'Content-Type': 'application/json'},
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((json) => {
+            switch (outputFormat) {
+              case "stream":
+                if (json.output) {
+                  console.log(nodeIdentifier + ':$', json.output);
+                }
+                if (json.error) {
+                  console.log(nodeIdentifier + ':&2$', json.error);
+                }
+                break;
+              case "stream+json":
+                console.log(JSON.stringify(json) + ",");
+                break;
+            }
+          });
+      };
+      if (schedulerPattern) {
+        const code = cronScript.compile(job, {
+          "gatewayUri": gatewayUri + "/chainCode/invoke",
+          "requestBody": requestBody,
+          "nodeIdentifier": nodeIdentifier
+        });
+        const callback = function (error, standardOutput, standardError) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(standardOutput, standardError);
+          }
+        };
+        cronScript.schedule(nodeIdentifier, schedulerPattern, code, callback);
+      } else {
+        // cronScript.execute(code, callback);
+        job();
+      }
+    }
+  },
+  install: function (clusterDefinition, options) {
+    console.log("hysh", "install", clusterDefinition, options);
+  },
+  instantiate: function (clusterDefinition, args) {
+    console.log("hysh", "instantiate", clusterDefinition, options);
+  }
 };
